6c1ce56e578419d878757078f62273bd253fa26c
Lars Lemkens <larslemkens@gmail.com>
2025-05-21 08:00:32 +0200

    Parallel routes

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,7 +1,20 @@
+import { getAvailableNewsMonths, getAvailableNewsYears } from "@/lib/news";
+import Link from "next/link";
+
 export default function ArchivePage() {
+  const links = getAvailableNewsYears();
+
   return (
-    <div>
-      <h1>Archive</h1>
-    </div>
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link}>
+              <Link href={`/archive/${link}`}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
